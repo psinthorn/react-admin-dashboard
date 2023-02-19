57731849f5401bce364f8230153aec0d350065da
Sinthorn <psinthorn@gmail.com>
2023-02-19 09:42:23 +0700

    finish themeSettigns function

--- src/theme.js
+++ src/theme.js
@@ -117,3 +117,81 @@ export const tokens = (mode) => ({
         },
     })
 })
+
+// MUI theme settings 
+export const themeSettings = (mode) => {
+    const colors = tokens(mode);
+
+    // return object 
+    return {
+        pallet: {
+            mode: mode,
+            ...(mode === "dark" ? {
+                primary: { main: colors.gray[500] },
+                secondary: { main: colors.greenAccent[500] },
+                neutral: { 
+                    dark: colors.gray[700],
+                    main: colors.gray[500],
+                    light: colors.gray[100]
+                 },
+                 background: {
+                    default: colors.primary[500]
+                 }
+            } : {
+                primary: { main: colors.gray[100] },
+                secondary: { main: colors.greenAccent[500] },
+                neutral: { 
+                    dark: colors.gray[700],
+                    main: colors.gray[500],
+                    light: colors.gray[100]
+                 },
+                 background: {
+                    default: "#fcfcfc"
+                 },
+            }),
+        },
+        typography: {
+            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 14,
+            }
+        }
+    }
+};
+
+// create context for color mode 
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark")
+
+    const useColor = useMemo(() => ({
+            toggleColorMode: () => 
+                setMode((prev) => (prev === "light" ? "dark" : "light" )),
+        }),[]
+    )
+};
